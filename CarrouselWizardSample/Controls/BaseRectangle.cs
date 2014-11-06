27891715831d57530a71c7b0ad7315b6f8ab9fc1
rfsouto <rfernandezsouto@gmail.com>
2014-11-06 18:10:58 +0100

    First Commit

---
+++ CarrouselWizardSample/Controls/BaseRectangle.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace CarrouselWizardSample.Controls
+{
+    public class BaseRectangle : ContentControl
+    {
+        private readonly Canvas canvas;
+        private readonly DoubleAnimation animation;
+        private readonly Storyboard storyboard;
+        private Border border;
+
+        public int SelectedIndex
+        {
+            get { return (int)this.GetValue(SelectedIndexProperty); }
+            set { this.SetValue(SelectedIndexProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedIndexProperty = DependencyProperty.Register(
+            "SelectedIndex",
+            typeof(int),
+            typeof(BaseRectangle),
+            new PropertyMetadata(0, new PropertyChangedCallback(OnSelectedIndexChanged)));
+
+        public int ItemsCount
+        {
+            get { return (int)this.GetValue(ItemsCountProperty); }
+            set { this.SetValue(ItemsCountProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsCountProperty = DependencyProperty.Register(
+            "ItemsCount",
+            typeof(int),
+            typeof(BaseRectangle),
+            new PropertyMetadata(1, new PropertyChangedCallback(OnItemsCountChanged)));
+
+        public Color RectangleColor
+        {
+            get { return (Color) this.GetValue(RectangleColorProperty); }
+            set { this.SetValue(RectangleColorProperty, value);}
+        }
+
+        public static readonly DependencyProperty RectangleColorProperty = DependencyProperty.Register(
+            "RectangleColor", 
+            typeof(Color), 
+            typeof(BaseRectangle), 
+            new PropertyMetadata(Color.FromArgb(255, 119, 95, 137),new PropertyChangedCallback(OnRectangleColorChanged)));
+
+        public BaseRectangle()
+        {
+            this.canvas = new Canvas
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+            };
+            this.Content = this.canvas;
+
+            this.animation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            this.storyboard = new Storyboard();
+            this.storyboard.Children.Add(this.animation);
+
+            this.Loaded += new RoutedEventHandler(this.OnCurrentPivotIndicatorLoaded);
+        }
+
+        private void OnCurrentPivotIndicatorLoaded(object sender, RoutedEventArgs e)
+        {
+            this.RebuildIndicator();
+        }
+
+        private void RebuildIndicator()
+        {
+            double actualWidth = base.ActualWidth;
+            double actualHeight = base.ActualHeight;
+
+            this.canvas.Width = actualWidth;
+            this.canvas.Height = actualHeight;
+            this.canvas.Children.Clear();
+
+            var translateTransform = new TranslateTransform();
+            this.border = new Border
+            {
+                Width = (int)(actualWidth / ((double)this.ItemsCount)),
+                Height = actualHeight,
+                BorderBrush = new SolidColorBrush(this.RectangleColor),
+                BorderThickness = new Thickness(0.0, actualHeight, 0.0, 0.0),
+                RenderTransform = translateTransform,
+                CacheMode = new BitmapCache()
+            };
+
+            this.storyboard.Stop();
+
+            // animate TranlateTransform.X property
+            // not that using TranslateTransform matters, a Canvas.Left animation for example will
+            // have less good performance because the compositor thread will not handle it
+            Storyboard.SetTarget(this.animation, translateTransform);
+            Storyboard.SetTargetProperty(this.animation, new PropertyPath(TranslateTransform.XProperty));
+
+            this.canvas.Children.Add(this.border);
+
+            this.ChangeSelectedIndex(0, this.SelectedIndex);
+        }
+
+        private void ChangeSelectedIndex(int oldIndex, int newIndex)
+        {
+            if (this.border != null)
+            {
+                double width = this.border.Width;
+                this.animation.From = width * oldIndex;
+                this.animation.To = width * newIndex;
+                this.storyboard.Begin();
+            }
+        }
+
+        private void OnRectangleColorChanged(DependencyPropertyChangedEventArgs e)
+        {
+            this.RebuildIndicator();
+        }
+
+        private void OnSelectedIndexChanged(DependencyPropertyChangedEventArgs e)
+        {
+            this.ChangeSelectedIndex((int)e.OldValue, (int)e.NewValue);
+        }
+
+        private void OnItemsCountChanged(DependencyPropertyChangedEventArgs e)
+        {
+            this.RebuildIndicator();
+        }
+
+        private static void OnSelectedIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((BaseRectangle)d).OnSelectedIndexChanged(e);
+        }
+
+        private static void OnItemsCountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((BaseRectangle)d).OnItemsCountChanged(e);
+        }
+
+        private static void OnRectangleColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((BaseRectangle)d).OnRectangleColorChanged(e);
+        }
+    }
+}
---
+++ CarrouselWizardSample/MainPage.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CarrouselWizardSample.Resources;
+using Microsoft.Xna.Framework.Input.Touch;
+using System.Threading.Tasks;
+using GestureEventArgs = Microsoft.Phone.Controls.GestureEventArgs;
+
+namespace CarrouselWizardSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        
+        int currentIndexOriginal = 0;
+        int currentIndex = 0;
+        TouchPoint _first;
+        private List<String> htmls = new List<String>();
+
+        List<String> ImagesList = new List<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadImages();
+            WizardIndicator.ItemsCount = ImagesList.Count;
+            WizardIndicator.SelectedIndex = 0;
+
+            TouchPanel.EnabledGestures = GestureType.HorizontalDrag;
+            NavigationInTransition navigateInTransition = new NavigationInTransition();
+            navigateInTransition.Backward = new TurnstileTransition { Mode = TurnstileTransitionMode.BackwardIn };
+            navigateInTransition.Forward = new TurnstileTransition { Mode = TurnstileTransitionMode.ForwardIn };
+
+            NavigationOutTransition navigateOutTransition = new NavigationOutTransition();
+            navigateOutTransition.Backward = new TurnstileTransition { Mode = TurnstileTransitionMode.BackwardOut };
+            navigateOutTransition.Forward = new TurnstileTransition { Mode = TurnstileTransitionMode.ForwardOut };
+            TransitionService.SetNavigationInTransition(this, navigateInTransition);
+            TransitionService.SetNavigationOutTransition(this, navigateOutTransition);
+            Touch.FrameReported += Touch_FrameReported;
+
+            
+        }
+
+        private void Touch_FrameReported(object sender, TouchFrameEventArgs e)
+        {
+
+            TouchPoint mainTouch = e.GetPrimaryTouchPoint(null);
+            Microsoft.Phone.Controls.PageOrientation currentOrientation = (App.Current.RootVisual as PhoneApplicationFrame).Orientation;
+            int numPivotItems = htmls.Count;
+
+            if (mainTouch.Action == TouchAction.Down)
+                _first = mainTouch;
+            else if (mainTouch.Action == TouchAction.Up && _first != null)
+            {
+                currentIndexOriginal = currentIndex;
+                var xDelta = mainTouch.Position.X - _first.Position.X;
+                var yDelta = Math.Abs(mainTouch.Position.Y - _first.Position.Y);
+                var yDeltaLand = mainTouch.Position.Y - _first.Position.Y;
+                bool movEsquerra = true;
+
+                if (yDelta > Math.Abs(xDelta)) return;
+                if (Math.Abs(xDelta - 0) > 30)
+                {
+                    if (xDelta < 0)
+                    {
+                        movEsquerra = true;
+                        if (currentIndex < WizardIndicator.ItemsCount-1)
+                        {
+                            currentIndex++;
+                        }
+                    }
+                    else
+                    {
+                        movEsquerra = false;
+                        if (currentIndex > 0)
+                        {
+                            currentIndex--;
+                        }
+                    }
+                }
+
+                WizardIndicator.SelectedIndex = currentIndex;
+                if (currentIndexOriginal != currentIndex)
+                {
+                    if (movEsquerra)
+                    {
+                        SlideTransition slideIni = new SlideTransition();
+                        slideIni.Mode = SlideTransitionMode.SlideLeftFadeOut;
+                        ITransition transitionIni = slideIni.GetTransition(ImageGrid);
+                        transitionIni.Completed += transition_CompletedIzquierdaDerecha;
+                        transitionIni.Begin();
+                    }
+                    else
+                    {
+                        SlideTransition slideIni = new SlideTransition();
+                        slideIni.Mode = SlideTransitionMode.SlideRightFadeOut;
+                        ITransition transitionIni = slideIni.GetTransition(ImageGrid);
+                        transitionIni.Completed += transition_CompletedDerechaIzquierda;
+                        transitionIni.Begin();
+                    }
+
+                }
+
+            }
+        }
+
+        private async void transition_CompletedDerechaIzquierda(object sender, EventArgs e)
+        {
+            ITransition transition = (ITransition)sender;
+            //Gestión de los botones interactivos sobre las imágenes. 
+
+            //Cambiar Imagen
+            ImageContainer.ImageSource = new BitmapImage(new Uri(ImagesList[currentIndex], UriKind.RelativeOrAbsolute));
+            
+            await Task.Delay(150);
+            transition.Stop();
+
+            SlideTransition slide = new SlideTransition();
+            slide.Mode = SlideTransitionMode.SlideRightFadeIn;
+
+            ITransition transitionfi = slide.GetTransition(ImageGrid);
+            transitionfi.Completed += delegate
+            {
+                transitionfi.Stop();
+            };
+
+            transitionfi.Begin();
+        }
+
+        public async void transition_CompletedIzquierdaDerecha(object sender, EventArgs e)
+        {
+            ITransition transition = (ITransition)sender;
+
+            //Cambiar Imagen
+            ImageContainer.ImageSource = new BitmapImage(new Uri(ImagesList[currentIndex], UriKind.RelativeOrAbsolute));
+            await Task.Delay(150);
+            transition.Stop();
+
+            SlideTransition slide = new SlideTransition();
+            slide.Mode = SlideTransitionMode.SlideLeftFadeIn;
+
+            ITransition transitionfi = slide.GetTransition(ImageGrid);
+            transitionfi.Completed += delegate
+            {
+                transitionfi.Stop();
+            };
+
+            transitionfi.Begin();
+        }
+
+        private void Exit()
+        {
+            Touch.FrameReported -= Touch_FrameReported;
+            /*TODO: EXIT EVENTS*/
+        }
+
+
+        private void btnAbrirTutorial_Click(object sender, RoutedEventArgs e)
+        {
+            currentIndex = 1;
+            WizardIndicator.SelectedIndex = currentIndex;
+            SlideTransition slideIni = new SlideTransition();
+            slideIni.Mode = SlideTransitionMode.SlideLeftFadeOut;
+
+            ITransition transitionIni = slideIni.GetTransition(ImageGrid);
+            transitionIni.Completed += transition_CompletedIzquierdaDerecha;
+            transitionIni.Begin();
+        }
+
+        private void LoadImages()
+        {
+            ImagesList = new List<string>()
+            {
+                "/Assets/Guide/0.png",
+                "/Assets/Guide/1.png",
+                "/Assets/Guide/2.png",
+                "/Assets/Guide/3.png",
+                "/Assets/Guide/4.png"
+            };
+            ImageContainer.ImageSource = new BitmapImage(new Uri(ImagesList[currentIndex], UriKind.RelativeOrAbsolute));
+        }
+    }
+}
